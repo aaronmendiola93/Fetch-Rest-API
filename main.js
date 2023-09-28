96cccc1d8dc31916a1b7ecdd4ce15c91c07c688e
aaronmendiola93 <icmaarongmendiola@gmail.com>
2023-09-27 22:24:16 -0600

    Se agregan los archivos al repo

---
+++ main.js
@@ -0,0 +1,38 @@
+function solicitud(datos) {
+    console.log('Funcion no asincrona');
+    return new Promise(resolve => setTimeout(resolve, datos));
+
+}
+// Declarando una funcion asincrona
+async function f() {
+    console.log('Inicio de funcion asincrona');
+
+    await solicitud(2000);
+    console.log('Terminamos ejecucion de funcion asincrona');
+}
+
+function bigFunction() {
+    console.log('Funcion normal ejecutada');
+    let result = 0;
+
+    for (let i = 0; i < 1e7; i++) {
+        result += i;
+    }
+    console.log('Funcio normal terminada', result);
+}
+
+/**f();
+bigFunction();
+*/
+
+const COUNTER_P = document.getElementById("counter");
+let counter = 0;
+
+document.getElementById('btn-counter').addEventListener('click', async (e) => {
+    await setTimeout(() => {
+        counter++;
+        COUNTER_P.textContent = counter;
+    }, 2000 )
+})
+
+
